c569eefed8f0c69884a5a30ef45538c3cd2a6428
Hieu Tran <hmtran.ae@gmail.com>
2019-01-25 15:35:36 -0500

    parallax for landing page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,4 +1,22 @@
 import React, { Component } from "react";
+import { Parallax, ParallaxLayer } from "react-spring/addons";
+import "../App.css";
+
+const Page = ({ offset, gradient, onClick }) => (
+  <React.Fragment>
+    <ParallaxLayer offset={offset} speed={0.2} onClick={onClick}>
+      <div className="slopeBegin" />
+    </ParallaxLayer>
+
+    <ParallaxLayer offset={offset} speed={0.6} onClick={onClick}>
+      <div className={`slopeEnd ${gradient}`} />
+    </ParallaxLayer>
+
+    <ParallaxLayer className="landing-page-text landing-page-number" offset={offset} speed={0.3}>
+      <span style={{paddingLeft: '250px'}}>0{offset + 1}</span>
+    </ParallaxLayer>
+  </React.Fragment>
+);
 
 class LandingPage extends Component {
   state = {
@@ -21,7 +39,9 @@ class LandingPage extends Component {
         isLoggedIn: true
       });
     }
-  }
+  };
+
+  scroll = to => this.parallax.scrollTo(to);
 
   render() {
     return (
@@ -50,7 +70,7 @@ class LandingPage extends Component {
             )}
           </div>
         </div>
-        <div className="ui vertical stripe segment">
+        {/* <div className="ui vertical stripe segment">
           <div className="ui middle aligned stackable grid container">
             <div className="row">
               <div className="eight wide column">das</div>
@@ -59,6 +79,19 @@ class LandingPage extends Component {
               <div className="six wide right floated column">dasdas</div>
             </div>
           </div>
+        </div> */}
+        <div style={{ background: "#dfdfdf" }}>
+          <Parallax
+            className="landing-page-container"
+            ref={node => (this.parallax = node)}
+            pages={3}
+            horizontal
+            scrolling={false}
+          >
+            <Page offset={0} gradient="pink" onClick={() => this.scroll(1)} />
+            <Page offset={1} gradient="teal" onClick={() => this.scroll(2)} />
+            <Page offset={2} gradient="tomato" onClick={() => this.scroll(0)} />
+          </Parallax>
         </div>
       </div>
     );
