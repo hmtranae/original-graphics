04e03c90d8322eaf0458f44e916ce50a1f15f6fa
Hieu Tran <hmtran.ae@gmail.com>
2019-02-01 09:39:30 -0500

    build for gallery and store styling

---
+++ build/precache-manifest.d295f9b4d3c0557e3675c114a53cc28e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b573cceb07de532bd86e",
+    "url": "/static/js/main.b573cceb.chunk.js"
+  },
+  {
+    "revision": "3b8f4cda6e5c991d79c0",
+    "url": "/static/js/1.3b8f4cda.chunk.js"
+  },
+  {
+    "revision": "b573cceb07de532bd86e",
+    "url": "/static/css/main.b809d7ca.chunk.css"
+  },
+  {
+    "revision": "3b8f4cda6e5c991d79c0",
+    "url": "/static/css/1.0f00e6ad.chunk.css"
+  },
+  {
+    "revision": "9b8ac105b3e44c29d9b6e4de4f5c4945",
+    "url": "/index.html"
+  }
+];
